a4a3dbfa27417e9c823ecf4c98f9774be6fd476f
liwiadzz <liwiaa7@gmail.com>
2025-06-09 18:33:36 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_cors import CORS
 from app.routes.recommend import recommend_bp 
 
 db = SQLAlchemy()
@@ -13,7 +14,7 @@ def create_app():
     app.config['SECRET_KEY'] = 'tajnehaslo'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///filmapp.db'
 
-
+    CORS(app)
     db.init_app(app)
     bcrypt.init_app(app)
     login_manager.init_app(app)

ae06f8979f0c8341e08330c5c2b6ee0ec54349ab
liwiadzz <liwiaa7@gmail.com>
2025-06-09 23:06:52 +0200

    Update __init__.py
    zmiana zeby dzialalo na bazie danych w chmurze

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,8 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_cors import CORS
 from app.routes.recommend import recommend_bp 
+import os
+from dotenv import load_dotenv
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
@@ -12,7 +14,9 @@ login_manager = LoginManager()
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'tajnehaslo'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///filmapp.db'
+    #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///filmapp.db'
+    load_dotenv()
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 
     CORS(app)
     db.init_app(app)
